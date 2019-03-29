07671d442f69375b1f2ede062ad0ad46dda0c828
danyloflafi18 <44213810+danyloflafi18@users.noreply.github.com>
2019-03-29 07:50:51 +0200

    lab 6 Stream File

---
+++ src/main/java/ua/lviv/iot/insurance/managers/InsuranceManager.java
@@ -0,0 +1,71 @@
+package ua.lviv.iot.insurance.managers;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ua.lviv.iot.insurance.models.Insurance;
+import ua.lviv.iot.insurance.models.InsuranceType;
+
+public class InsuranceManager implements SortOfMethodsInsurance {
+
+    private List<Insurance> insurance = new LinkedList<>();
+
+    public InsuranceManager() {
+    }
+
+    public InsuranceManager(final List<Insurance> insurance) {
+        this.insurance = insurance;
+    }
+
+    public final List<Insurance> getInsurance() {
+        return insurance;
+    }
+
+    public final void setInsurance(final List<Insurance> insurance) {
+        this.insurance = insurance;
+    }
+
+    @Override
+    public final List<Insurance> sortingBasedOnRiskReduction(
+            final boolean descending) {
+        Comparator<Insurance> comparator = (Insurance obj1,
+                Insurance obj2) -> Integer.compare(obj2.getNumberOfDays(),
+                obj1.getNumberOfDays());
+        insurance.sort(comparator);
+        return insurance;
+    }
+
+    @Override
+    public final List<Insurance> sortingAlphabeticallyInAscendingOrder(
+            final boolean descending) {
+        Comparator<Insurance> comparator = (Insurance obj1,
+                Insurance obj2) -> obj1.getName().compareTo(obj2.getName());
+        insurance.sort(comparator);
+        return insurance;
+    }
+
+    @Override
+    public final List<Insurance> sortingAlphabeticallyInDescendingOrder(
+            final boolean descending) {
+        Comparator<Insurance> comparator = (Insurance obj1,
+                Insurance obj2) -> obj2.getName().compareTo(obj1.getName());
+        insurance.sort(comparator);
+        return insurance;
+    }
+
+    @Override
+    public final List<Insurance> findBy(final InsuranceType type) {
+        return insurance.stream().filter(ins -> ins.getInsuranceType() == type)
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    public final void addInsurance(final Insurance insurance) {
+        insurance.add(insurance);
+
+    }
+
+}
---
+++ src/main/java/ua/lviv/iot/insurance/models/Health.java
@@ -0,0 +1,59 @@
+package ua.lviv.iot.insurance.models;
+
+
+public class Health extends Insurance {
+
+    private boolean prematureDeath;
+    private DangerLevel accident;
+    private DangerLevel invalidity;
+
+    public Health() {
+    }
+
+    public Health(final String name, final String surname,
+            final int numberOfDays, final String telephone,
+            final InsuranceType insuranceType, final boolean prematureDeath,
+            final DangerLevel accident) {
+        super(name, surname, numberOfDays, telephone, insuranceType);
+        this.prematureDeath = prematureDeath;
+        this.accident = accident;
+    }
+
+    public final boolean isPrematureDeath() {
+        return prematureDeath;
+    }
+
+    public final void setPrematureDeath(final boolean prematureDeath) {
+        this.prematureDeath = prematureDeath;
+    }
+
+    public final DangerLevel getAccident() {
+        return accident;
+    }
+
+    public final void setAccident(final DangerLevel accident) {
+        this.accident = accident;
+    }
+
+    public final DangerLevel getInvalidity() {
+        return invalidity;
+    }
+
+    public final void setInvalidity(final DangerLevel invalidity) {
+        this.invalidity = invalidity;
+    }
+
+    @Override
+    public final String getHeaders() {
+        return super.getHeaders() + "," + "Premature death" + ","
+                + "Accident" + "," + "Invalidity";
+    }
+
+    @Override
+    public final String toCSV() {
+        return super.toCSV() + "," + this.isPrematureDeath() + ","
+                + this.getAccident() + "," + this.getInvalidity();
+    }
+
+
+}
